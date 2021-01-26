7ca15db85c9b1acab56878f6691cc75b699f6fee
ivan <ivanrubioangeles@gmail.com>
2021-01-25 19:34:06 -0600

    banner image

--- app/components/Header/Img.js
+++ app/components/Header/Img.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 import NormalImg from 'components/Img';
-
 const Img = styled(NormalImg)`
   width: 100%;
   margin: 0 auto;
   display: block;
 `;
+console.log(Img);
 
 export default Img;
